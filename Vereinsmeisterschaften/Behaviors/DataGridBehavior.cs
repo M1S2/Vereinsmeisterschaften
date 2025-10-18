e5fe11e5f156e390fad25aadd04bf8da48e99320
Markus Scheich <markus.scheich@gmx.de>
2025-10-18 20:15:27 +0200

    Added threestate sorting behavior for DataGrids, added some SortMemberPaths to some DataGridColumns

--- Vereinsmeisterschaften/Behaviors/DataGridBehavior.cs
+++ Vereinsmeisterschaften/Behaviors/DataGridBehavior.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows;
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace Vereinsmeisterschaften.Behaviors
 {
@@ -129,6 +130,103 @@ namespace Vereinsmeisterschaften.Behaviors
                     }
                 });
             }
+        }
+
+        #endregion
+
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        #region ThreeStateSort
+
+        /// <summary>
+        /// Attached property to enable three state sorting (Ascending, Descending, Disabled) and MultiColumn sorting with Shift key in a DataGrid.
+        /// </summary>
+        public static readonly DependencyProperty EnableThreeStateSortingProperty =
+            DependencyProperty.RegisterAttached("EnableThreeStateSorting",
+                                                typeof(bool),
+                                                typeof(DataGridBehavior),
+                                                new PropertyMetadata(false, OnEnableThreeStateSortingChanged));
+
+        /// <summary>
+        /// Gets the value of the EnableThreeStateSorting attached property from a DependencyObject.
+        /// </summary>
+        /// <param name="obj"><see cref="DependencyObject"/></param>
+        /// <returns>Value of the EnableThreeStateSorting attached property</returns>
+        public static bool GetEnableThreeStateSorting(DependencyObject obj)
+            => (bool)obj.GetValue(EnableThreeStateSortingProperty);
+
+        /// <summary>
+        /// Sets the value of the EnableThreeStateSorting attached property on a DependencyObject.
+        /// </summary>
+        /// <param name="obj"><see cref="DependencyObject"/></param>
+        /// <param name="value">Value for the EnableThreeStateSorting attached property</param>
+        public static void SetEnableThreeStateSorting(DependencyObject obj, bool value)
+            => obj.SetValue(EnableThreeStateSortingProperty, value);
+
+        private static void OnEnableThreeStateSortingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not DataGrid dataGrid)
+                return;
+
+            if ((bool)e.NewValue)
+                dataGrid.Sorting += OnDataGridSorting;
+            else
+                dataGrid.Sorting -= OnDataGridSorting;
+        }
+
+        // Code was generated with the help of ChatGPT
+        private static void OnDataGridSorting(object sender, DataGridSortingEventArgs e)
+        {
+            DataGrid grid = (DataGrid)sender;
+            string path = e.Column.SortMemberPath;
+            if (string.IsNullOrEmpty(path)) { return; }
+
+            bool shiftPressed = Keyboard.Modifiers.HasFlag(ModifierKeys.Shift);
+            e.Handled = true;
+
+            // Get the new sort direction based on the current sort direction
+            ListSortDirection? newDirection = e.Column.SortDirection switch
+            {
+                null => ListSortDirection.Ascending,
+                ListSortDirection.Ascending => ListSortDirection.Descending,
+                ListSortDirection.Descending => null,
+                _ => null
+            };
+
+            // If Shift is NOT pressed → clear all previous sortings
+            if (!shiftPressed)
+            {
+                foreach (DataGridColumn c in grid.Columns)
+                {
+                    if (!ReferenceEquals(c, e.Column))
+                    {
+                        c.SortDirection = null;
+                    }
+                }
+                grid.Items.SortDescriptions.Clear();
+            }
+
+            // Remove the column from SortDescriptions if it exists
+            SortDescription existingSortDescription = grid.Items.SortDescriptions.FirstOrDefault(sd => sd.PropertyName == path);
+            if (!string.IsNullOrEmpty(existingSortDescription.PropertyName))
+            {
+                grid.Items.SortDescriptions.Remove(existingSortDescription);
+            }
+
+            if (newDirection != null)
+            {
+                // Add new SortDescription with the new direction
+                grid.Items.SortDescriptions.Add(new SortDescription(path, newDirection.Value));
+                e.Column.SortDirection = newDirection;
+            }
+            else
+            {
+                // No sorting → reset column
+                e.Column.SortDirection = null;
+            }
+
+            // Apply sorting
+            grid.Items.Refresh();
         }
 
         #endregion

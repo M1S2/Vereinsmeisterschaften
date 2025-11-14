35c9b1d60d90f4a9e6b2a10ac2170eb1a0b9494e
Markus Scheich <markus.scheich@gmx.de>
2025-11-14 22:36:31 +0100

    First not-really-working MultiSelectComboBox generated by ChatGPT

---
+++ Vereinsmeisterschaften/Controls/MultiSelectComboBox.xaml.cs
@@ -0,0 +1,86 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Vereinsmeisterschaften.Controls
+{
+    public partial class MultiSelectComboBox : UserControl
+    {
+        public MultiSelectComboBox()
+        {
+            InitializeComponent();
+            SelectedItems = new ObservableCollection<object>();
+        }
+
+        // ItemsSource
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(nameof(ItemsSource), typeof(IEnumerable), typeof(MultiSelectComboBox), new PropertyMetadata(null));
+
+        // SelectedItems
+        public ObservableCollection<object> SelectedItems
+        {
+            get => (ObservableCollection<object>)GetValue(SelectedItemsProperty);
+            set => SetValue(SelectedItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.Register(nameof(SelectedItems), typeof(ObservableCollection<object>), typeof(MultiSelectComboBox), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        // ItemTemplate
+        public DataTemplate ItemTemplate
+        {
+            get => (DataTemplate)GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register(nameof(ItemTemplate), typeof(DataTemplate), typeof(MultiSelectComboBox), new PropertyMetadata(null));
+
+
+        private void OnTogglePopup(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (PART_Popup != null)
+                PART_Popup.SetCurrentValue(Popup.IsOpenProperty, !PART_Popup.IsOpen);
+        }
+
+        private void OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox cb && cb.DataContext != null)
+            {
+                if (!SelectedItems.Contains(cb.DataContext))
+                    SelectedItems.Add(cb.DataContext);
+            }
+        }
+
+        private void OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox cb && cb.DataContext != null)
+            {
+                if (SelectedItems.Contains(cb.DataContext))
+                    SelectedItems.Remove(cb.DataContext);
+            }
+        }
+
+        // Command für Entfernen eines Chips
+        public ICommand RemoveItemCommand => new RelayCommand<object>(RemoveItem);
+
+        private void RemoveItem(object item)
+        {
+            if (item != null && SelectedItems.Contains(item))
+            {
+                SelectedItems.Remove(item);
+            }
+        }
+    }
+}
---
+++ Vereinsmeisterschaften/Converters/SelectedItemsContainsConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Vereinsmeisterschaften.Converters
+{
+    public class SelectedItemsContainsConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length >= 2 &&
+                values[0] is IList list &&
+                values[1] != null)
+            {
+                return list.Contains(values[1]);
+            }
+
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
